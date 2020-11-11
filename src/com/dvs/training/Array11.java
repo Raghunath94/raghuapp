3db3bd55c58b0f92c404cf104a416d9ca54f9450
Raghunath <panchangam94@gmail.com>
2020-11-11 18:56:55 +0530

    ssome changes

--- src/com/dvs/training/Array11.java
+++ src/com/dvs/training/Array11.java
@@ -6,20 +6,20 @@ public static void main(String[] args) {
 
 	        int[] numbers = {2, -9, 0, 5, 12, -25, 22, 9, 8, 12};
 	        int sum = 0;
-	        Double average;
+	      //  Double average;
 	     
 	        // for each loop is used to access elements 
 	        for (int number: numbers) {
 	            sum += number;
 	        }
-	     
-	        int arrayLength = numbers.length;
+	        
+	//        arrayLength = numbers.length;
 
 	        // Change sum and arrayLength to double as average is in double
-	        average =  ((double)sum / (double)arrayLength);
+	    //    average =  ((double)sum / (double)arrayLength);
 
 	        System.out.println("Sum = " + sum);
-	        System.out.println("Average = " + average);
+	     //   System.out.println("Average = " + average);
 	        int[] age = new int[5];
 
 	        // insert 14 to third element
